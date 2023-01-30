f1153880e2c7ddd03daa01b69774f9928cb7045c
Yaniv Ben Hemo <Yaniv@memphis.dev>
2023-01-30 13:53:58 +0200

    init

--- event-driven/dotnet/ProductOffer.WebAPI/Services/Commands/SendProductOfferDetailsCommand 3.cs
+++
@@ -1,7 +0,0 @@
-﻿using MediatR;
-using ProductOffer.WebAPI.Models;
-
-namespace ProductOffer.WebAPI.Services.Commands
-{
-    public record SendProductOfferDetailsCommand(SendProductOfferDetails SendProductOfferDetails) : IRequest;
-}
--- event-driven/dotnet/ProductOffer.WebAPI/Services/Queries/GetProductOfferDetailsQuery 3.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-using MediatR;
-
-namespace ProductOffer.WebAPI.Services.Queries
-{
-    public sealed record GetProductOfferDetailsQuery() : IRequest<List<Models.ProductOfferDetails>>;
-
-    public sealed record GetProductOfferDetailsByProductIdQuery(string productId) : IRequest<Models.ProductOfferDetails>;
-}
